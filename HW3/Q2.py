67a131f12ff26d4a8e40aee7294f63d65a4dd736
Chan-Wei Hu <huchanwei123@Chan-Weis-MacBook-Pro.local>
2020-10-13 19:12:53 -0500

    update

--- HW3/Q2.py
+++ HW3/Q2.py
@@ -9,16 +9,24 @@ import numpy as np
 
 def read_csv(csv_path):
     # create a dictionary
-    
+    dataset = []
     with open(csv_path) as f:
         reader = csv.DictReader(f)
         for row in reader:
             label = int(row['emotion'])
             pixel = np.array(row['pixels'].split()).astype(np.uint8)
-            
+
             # convert to 48x48 image
             img = np.reshape(pixel, (48, 48))
 
-    
+            # append to dataset
+            # dataset[n][0] is the emotion label
+            # dataset[n][1] is the image
+            dataset.append([label, img])
 
-read_csv('./Q2_Train_Data.csv')
+            # convert to numpy array
+            np_dataset = np.asarray(dataset)
+
+    return np_dataset
+
+read_csv('../../hw3_data/Q2_Train_Data.csv')
